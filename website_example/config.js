fab8b0ef6da7b38018c53992232be181650b6e87
Latin Cash <34460610+LatinCash@users.noreply.github.com>
2018-06-01 09:49:18 -0300

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -1,13 +1,13 @@
-var api = "http://poolhost:8117";
+var api = "http://45.63.16.80:8117";
 
-var email = "support@poolhost.com";
+var email = "email@pool.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://latincashexplorer.ddns.net/?hash={id}#blockchain_block";
+var transactionExplorer = "http://latincashexplorer.ddns.net/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
