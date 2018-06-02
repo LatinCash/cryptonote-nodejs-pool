8d67392f76ba6ed608c886f5bf70e2d5d48bb40b
LatinCash <34460610+LatinCash@users.noreply.github.com>
2018-06-02 00:01:33 -0300

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -2,7 +2,7 @@ var api = "http://45.63.16.80:8117";
 
 var email = "email@pool.com";
 var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
+var discord = "https://discordapp.com/invite/keez2rW";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
